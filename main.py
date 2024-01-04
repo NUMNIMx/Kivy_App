7e867e1ef846b925a692f967cf45fe94be5ac551
NUMNIMx <itspeemgmth@gmail.com>
2024-01-04 22:10:47 +0700

    login and reset screen sucess

---
+++ main.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.properties import StringProperty
+from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
+import os
+
+from connected import Connected
+
+class Login(Screen):
+    def do_login(self, loginText, passwordText):
+        app = App.get_running_app()
+
+        app.username = loginText
+        app.password = passwordText
+
+        self.manager.transition = SlideTransition(direction="left")
+        self.manager.current = 'connected'
+
+        app.config.read(app.get_application_config())
+        app.config.write()
+
+    def resetForm(self):
+        self.ids['login'].text = ""
+        self.ids['password'].text = ""
