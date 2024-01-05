c00789317923353d5e9ae8cff199dfa17f71f9bc
NUMNIMx <itspeemgmth@gmail.com>
2024-01-05 22:12:35 +0700

    main full complate

--- main.py
+++ main.py
@@ -21,3 +21,31 @@ class Login(Screen):
     def resetForm(self):
         self.ids['login'].text = ""
         self.ids['password'].text = ""
+
+class LoginApp(App):
+    username = StringProperty(None)
+    password = StringProperty(None)
+
+    def build(self):
+        manager = ScreenManager()
+
+        manager.add_widget(Login(name='login'))
+        manager.add_widget(Connected(name='connected'))
+
+        return manager
+
+    def get_application_config(self):
+        if(not self.username):
+            return super(LoginApp, self).get_application_config()
+
+        conf_directory = self.user_data_dir + '/' + self.username
+
+        if(not os.path.exists(conf_directory)):
+            os.makedirs(conf_directory)
+
+        return super(LoginApp, self).get_application_config(
+            '%s/config.cfg' % (conf_directory)
+        )
+
+if __name__ == '__main__':
+    LoginApp().run()
