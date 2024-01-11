d4302742529e25e528084c94078abd03eef173f6
NUMNIMx <itspeemgmth@gmail.com>
2024-01-11 20:55:14 +0700

    change net speed test to paint tool

--- paint_tool.py
+++ paint_tool.py
@@ -9,12 +9,12 @@ from kivy.graphics import Line, Color, Ellipse
 class PaintWidget(Widget):
     def __init__(self, **kwargs):
         super(PaintWidget, self).__init__(**kwargs)
-        self.color = (1, 1, 1, 1)  # สีเริ่มต้นของปัญหา
+        self.color = (1, 1, 1, 1)  # Initial color of the paintbrush
 
     def on_touch_down(self, touch):
         with self.canvas:
             Color(*self.color)
-            d = 30  # ขนาดของปลายปากกา
+            d = 10  # Size of the paintbrush tip
             Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
             touch.ud['line'] = Line(points=(touch.x, touch.y), width=2)
 
@@ -25,11 +25,10 @@ class PaintWidget(Widget):
 class PaintApp(App):
     def build(self):
         self.paint_widget = PaintWidget()
-
         color_picker = ColorPicker()
         color_picker.bind(color=self.on_color)
 
-        clear_button = Button(text='Clear')
+        clear_button = Button(text='Clear', size_hint=(None, None), size=(100, 50))
         clear_button.bind(on_release=self.clear_canvas)
 
         layout = BoxLayout(orientation='vertical')
